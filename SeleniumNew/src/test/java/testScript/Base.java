d52e6705d4e8f5fe2ca29eedf5cb6e0b7c61efa8
PreethiArun <160821896+preethiprasobh@users.noreply.github.com>
2024-05-23 14:30:50 +0530

    Base class updated

--- SeleniumNew/src/test/java/testScript/Base.java
+++ SeleniumNew/src/test/java/testScript/Base.java
@@ -2,20 +2,23 @@ package testScript;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;//Add the dependencies for selenium in pom.xml file
 
 public class Base {
-	public WebDriver driver;//global access
+	public WebDriver driver;//global access //WebDriver is an interface that provides methods for controlling web browsers
 	public void initializeBrowser()
 	{
-		driver=new ChromeDriver();//loading the chrome driver (or) browser initialization
+		driver=new ChromeDriver();//loading the chrome driver (or) browser initialization(ctrl+space)//initializes the WebDriver instance by creating a new ChromeDriver object
 		driver.get("https://selenium.qabible.in/");//launch the URL//wait till the whole page loads
 		driver.manage().window().maximize();
 		
 				}
 public void driverQuitandClose()//Quit-closing both parent and associated windows, close-closing only parent window
 {
-	//driver.close();
-	driver.quit();
+	//driver.close();-If there is only one browser window open, it will close that window and end the WebDriver session.
+	//If there are multiple windows or tabs open, it will close the currently active window or tab without terminating the entire WebDriver session
+	driver.quit();//closes all browser windows and terminates the WebDriver session completely.
 }
 
 	public static void main(String[] args) {
